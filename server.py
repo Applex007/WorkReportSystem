adb051f42eaf0c531705f8dfb1a813accdbcad3f
13167178850 <13167178850@163.com>
2018-03-28 02:10:44 +0800

    init project

---
+++ server.py
@@ -0,0 +1,71 @@
+#coding=utf-8
+#@auth: applex
+#@date: 2018-03-27
+
+import socket
+import thread
+
+
+class Server:
+    def __init__(self, message_callback):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.settimeout(100)
+        self.host = socket.gethostname()
+        self.port = 7777
+        self.startFlag = False
+        self.message_callback = message_callback
+
+
+    def __del__(self):
+        self.startFlag = False
+        self.s.close()
+
+
+    def bind(self):
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind((self.host, self.port))
+        self.s.listen(5)
+        self.startFlag = True
+        try:
+            thread.start_new_thread(self.accept, ("socket.accept_thread", 1))
+            # self.accept("accept", 1)
+            return True
+        except Exception as e:
+            print "Server: socket bind, start accept thread exception", e
+            return False
+
+
+    def destroy(self):
+        self.startFlag = False
+        try:
+            # self.s.shutdown(2)
+            self.s.close()
+            return True
+        except Exception as e:
+            print "error", e
+            return False
+
+
+    def accept(self, thread_id, delay):
+        print "socket: accept", thread_id, delay
+        while self.startFlag:
+            print "socket: accept", self.startFlag
+            conn, address = self.s.accept()
+            message = conn.recv(1024)
+            self.handle_message(conn, address, message)
+            conn.close()
+        print "socket: shutdown", self.startFlag
+
+
+    def handle_message(self, conn, address, message):
+        print "socket: handle_message", conn, address, message
+        self.message_callback("来自客户端：%s的消息：%s" %(address, message))
+
+
+
+
+
+
+
+
+
---
+++ serverUI.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+# @auth: applex
+# @date: 2018-03-27
+
+from Tkinter import *
+from server import *
+
+STR_SERVER_START = "点击启动服务"
+STR_SERVER_STOP = "点击终止服务"
+ServerState = False
+
+
+def server_button_clicked():
+    global ServerState
+    ServerState = ~ServerState
+    print "ServerState", ServerState
+    if ServerState:
+        start_server_thread()
+    else:
+        stop_server_thread()
+
+
+def start_server_thread():
+    print "start_server_thread", ServerState
+    if server.bind():
+        serverButton.config(text=STR_SERVER_STOP)
+        serverInfoLabel.config(text="服务创建成功...")
+    else:
+        serverInfoLabel.config(text="服务创建失败...")
+
+
+def stop_server_thread():
+    print "stop_server_thread", ServerState
+    if server.destroy():
+        serverButton.config(text=STR_SERVER_START)
+        serverInfoLabel.config(text="服务已终止！")
+
+
+def show_message(message):
+    infoText.insert(END, message)
+
+
+server = Server(show_message)
+root = Tk()
+root.geometry('800x800')
+root.resizable(width='false', height='false')
+
+infoLabel1 = Label(root, text="消息提示：")
+infoLabel1.grid(row=0, column=0, padx=10, pady=10, sticky=W)
+
+serverButton = Button(root, text=STR_SERVER_START, command=server_button_clicked)
+serverButton.grid(row=0, column=1, padx=10, pady=10, sticky=E)
+
+serverInfoLabel = Label(root)
+serverInfoLabel.grid(row=0, column=0, padx=10, pady=10, sticky=E)
+
+infoText = Text(root, bg='gray', width=100, height=30)
+infoText.grid(row=1, columnspan=2, padx=10, pady=10)
+
+root.mainloop()
